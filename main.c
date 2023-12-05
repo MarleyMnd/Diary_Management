b4ad146090da66ae790f8b6f409fffe7b9af5400
Marley <mrl.menard@gmail.com>
2023-12-05 19:28:20 +0100

    random values + levels

--- main.c
+++ main.c
@@ -6,23 +6,23 @@
 int main() {
 
     int max_list_level;
-    printf("Enter the max number of levels of the list : ");
+    printf("Enter the max number of levels of the list :");
     scanf("%d", &max_list_level);
     printf("\n");
 
     int NumberOfCells;
-    printf("Enter the number of cells of your level list : ");
+    printf("Enter the number of cells of your level list :");
     scanf("%d", &NumberOfCells);
     printf("\n");
 
     t_d_list level_list = createList(max_list_level);
 
     srand(time(NULL));
-    int values[10] = {};
+    int values[100] = {};
     for (int i = 0; i < NumberOfCells; i++) {
         values[i] = rand() % 100;
     }
-    printf("Values :");
+    printf("Values of the list :");
     for (int i = 0; i < NumberOfCells; i++) {
         printf("[%d]  ", values[i]);
     }
@@ -33,8 +33,17 @@ int main() {
     insertCellHead(&level_list, /*value*/ values[0], /*number of levels of the cell*/ number_levels_first_cell);
 
     int j = 1;
+    int number_levels_cell[100] = {};
+    for (int i = 0; i < NumberOfCells; i++) {
+        number_levels_cell[i] = rand() % max_list_level + 1;
+    }
+    printf("Number of levels of the cells :");
+    for (int i = 0; i < NumberOfCells; i++) {
+        printf("[%d]  ", number_levels_cell[i]);
+    }
+    printf("\n\n");
     for (int i = 1; i < NumberOfCells; i++) {
-        insertCellAscendingOrder(&level_list, /*value*/ values[j], /*number of level of the cell*/ 2, max_list_level);
+        insertCellAscendingOrder(&level_list, /*value*/ values[j], /*number of level of the cell*/ number_levels_cell[i], max_list_level);
         j++;
     }
 
